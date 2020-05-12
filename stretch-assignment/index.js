bce578d406ebb1a000c8017074efa0d899bd3f1a
Sawyer Pearson <sawyer.pearson92@gmail.com>
2020-05-11 19:17:49 -0600

    won't have time to finish stretch, pushing project

--- stretch-assignment/index.js
+++ stretch-assignment/index.js
@@ -1,24 +1,43 @@
 let allDivs = document.querySelectorAll('.blocks div')
 let blockContainer = document.querySelector('.blocks')
-
-// allDivs.forEach(divs => {
-//     divs.addEventListener('click', (event) => {
-//         evnt.blockContainer.prependChild('<div>this is a test</div>');
-//     })
-// })
-
-
-const grayShip = document.querySelector('.block--gray');
-
-// grayShip.addEventListener('click', (event) => {
-//     blockContainer.prepend(grayShip)
-// })
+let activeDiv = document.getElementById('activeShip')
+let topDiv = document.querySelector('.block')
 
 allDivs.forEach(divs => {
     divs.addEventListener('click', (event) => {
+        divs.setAttribute('id', 'activeShip');
+        divs.setAttribute('onclick', 'moveRight()')
         blockContainer.prepend(divs)
-    })
+    }) 
 })
 
+// activeDiv.addEventListener('mousedown', (event) => {
+//     currentPOS = currentPOS+10;
 
+//     event.target.style.right = currentPOS + 'px';
+// })
+
+function moveRight() {
+    if ('mousedown' === true) {
+        // currentPOS = currentPOS+10;
+        // activeDiv.style.right = currentPOS + 'px';
+        activeDiv.target.style.display = 'none';
+    }
+}
+
+
+
+// activeDiv.addEventListener('mousedown', (event) => {
+    
+// })
+
+
+
+// activeDiv.forEach(divs => {
+//     divs.addEventListener('mousedown', (event) => {
+//         event.target.style.marginLeft = "50px";
+//     }) 
+// })
+
+//on mouseup - move div back to start
 
